9863dd58295e2e8fcbf87c7d5f2ae876cf5fa462
Mohammed elkurd <moh.elkurd@gmail.com>
2026-09-15 13:52:06 +0300

    perf: use lightweight landing favicon

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -189,7 +189,7 @@
     </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ config('app.icon') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     @php
         $actualDesc = $metaDescription ?? '';
